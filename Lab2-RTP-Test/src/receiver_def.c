75496cd0c1502d23538a096b258f4b74d3087698
leojjw <leojjw@naver.com>
2022-11-14 23:14:20 -0800

    Update

---
+++ Lab2-RTP-Test/src/receiver_def.c
@@ -0,0 +1,187 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "sys/socket.h"
+#include "netdb.h"
+#include "arpa/inet.h"
+#include "string.h"
+#include "receiver_def.h"
+#include "rtp.h"
+#include "util.h"
+
+struct sockaddr_in sendaddr;
+int sockfd_receiver;
+uint32_t N_receiver;
+
+void sendACK(uint32_t seq_num){
+    struct RTP_header header;
+
+    header.type = RTP_ACK;
+    header.length = 0;
+    header.seq_num = seq_num;
+    header.checksum = 0;
+    header.checksum = compute_checksum(&header, sizeof(header));
+
+    sendto(sockfd_receiver, &header, sizeof(header), 0, (struct sockaddr *)&sendaddr, sizeof(sendaddr));
+}
+
+int initReceiver(uint16_t port, uint32_t window_size){
+    struct sockaddr_in recvaddr;
+    struct RTP_header header;
+    int len = sizeof(sendaddr);
+
+    sockfd_receiver = socket(AF_INET, SOCK_DGRAM, 0);
+    struct timeval tv;
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
+    setsockopt(sockfd_receiver, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+    bzero(&recvaddr, sizeof(recvaddr));
+    recvaddr.sin_family = AF_INET;
+    recvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    recvaddr.sin_port = htons(port);
+    bind(sockfd_receiver, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
+
+    recvfrom(sockfd_receiver, &header, sizeof(header), 0, (struct sockaddr *)&sendaddr, &len);
+    uint32_t checksum = header.checksum;
+    header.checksum = 0;
+    if ((header.type == RTP_START) && (checksum == compute_checksum(&header, sizeof(header)))){
+        header.type = RTP_ACK;
+        header.checksum = compute_checksum(&header, sizeof(header));
+        sendto(sockfd_receiver, &header, sizeof(header), 0, (struct sockaddr *)&sendaddr, sizeof(sendaddr));
+        N_receiver = window_size;
+        return 0;
+    }
+    return -1;
+}
+
+int recvMessage(char* filename){
+    FILE *fp = fopen(filename, "wb");
+
+    struct RTP_packet packet;
+    uint32_t expectseqnum = 0, rcvdseqnum, checksum;
+    int received_bytes = 0;
+    uint8_t* ack_num = (uint8_t*)malloc(sizeof(uint8_t)*(N_receiver-1));
+    memset(ack_num, 0, N_receiver);
+    
+    char *buffer = (char*)malloc(sizeof(char)*(PAYLOAD_SIZE * (N_receiver-1)));
+    int *buffer_length = (int*)malloc(sizeof(int)*(N_receiver-1));
+    ssize_t ret, len, b;
+
+    while(1){
+        ret = recvfrom(sockfd_receiver, &packet, sizeof(packet), 0, NULL, NULL);
+        rcvdseqnum = packet.rtp.seq_num;
+        checksum = packet.rtp.checksum;
+        packet.rtp.checksum = 0;
+        if (packet.rtp.type == RTP_DATA){
+            ret -= sizeof(struct RTP_header);
+            len = packet.rtp.length;
+            if (checksum == compute_checksum(&packet, sizeof(struct RTP_header) + len)){
+                if (rcvdseqnum == expectseqnum){
+                    received_bytes += ret;
+                    fwrite(packet.payload, sizeof(char), len, fp);
+                    expectseqnum += 1;
+                    for (int i = 1; i < N_receiver; i++){
+                        if (ack_num[expectseqnum % (N_receiver-1)]){
+                            ack_num[expectseqnum % (N_receiver-1)] = 0;
+                            fwrite(buffer + ((expectseqnum % (N_receiver-1)) * PAYLOAD_SIZE), sizeof(char), buffer_length[expectseqnum % (N_receiver-1)], fp);
+                        } else {
+                            break;
+                        }
+                        expectseqnum += 1;
+                    }
+                    sendACK(expectseqnum);
+                } else if (rcvdseqnum > expectseqnum && rcvdseqnum < (expectseqnum + N_receiver)) {
+                    if (!ack_num[rcvdseqnum % (N_receiver-1)]){
+                        memcpy(buffer + ((rcvdseqnum % (N_receiver-1)) * PAYLOAD_SIZE), packet.payload, len);
+                        received_bytes += ret;
+                        ack_num[rcvdseqnum % (N_receiver-1)] = 1;
+                        buffer_length[rcvdseqnum % (N_receiver-1)] = len;
+                        sendACK(expectseqnum);
+                    }
+                } else if (rcvdseqnum < expectseqnum){
+                    sendACK(expectseqnum);
+                }
+            }
+        } else if ((packet.rtp.type == RTP_END) && (checksum == compute_checksum(&packet, sizeof(struct RTP_header)))) {
+            sendACK(packet.rtp.seq_num);
+            close(sockfd_receiver);
+            fclose(fp);
+            return received_bytes;
+        } else if (ret == -1){
+            fclose(fp);
+            return -1;
+        }
+    }
+    fclose(fp);
+    return -1;
+}
+
+int recvMessageOpt(char* filename){
+    FILE *fp = fopen(filename, "wb");
+
+    struct RTP_packet packet;
+    uint32_t expectseqnum = 0, rcvdseqnum, checksum;
+    int received_bytes = 0;
+    uint8_t* ack_num = (uint8_t*)malloc(sizeof(uint8_t)*(N_receiver-1));
+    memset(ack_num, 0, N_receiver-1);
+    
+    char *buffer = (char*)malloc(sizeof(char)*(PAYLOAD_SIZE * (N_receiver-1)));
+    int *buffer_length = (int*)malloc(sizeof(int)*(N_receiver-1)), idx;
+    ssize_t ret, len;
+
+    while(1){
+        ret = recvfrom(sockfd_receiver, &packet, sizeof(packet), 0, NULL, NULL);
+        rcvdseqnum = packet.rtp.seq_num;
+        checksum = packet.rtp.checksum;
+        packet.rtp.checksum = 0;
+        if (packet.rtp.type == RTP_DATA){
+            ret -= sizeof(struct RTP_header);
+            len = packet.rtp.length;
+            if (checksum == compute_checksum(&packet, sizeof(struct RTP_header) + len)){
+                if (rcvdseqnum == expectseqnum){
+                    received_bytes += ret;
+                    fwrite(packet.payload, sizeof(char), len, fp);
+                    sendACK(rcvdseqnum);
+                    expectseqnum += 1;
+                    for (int i = 1; i < N_receiver; i++){
+                        idx = expectseqnum % (N_receiver-1);
+                        if (ack_num[idx]){
+                            ack_num[idx] = 0;
+                            fwrite(buffer + (idx * PAYLOAD_SIZE), sizeof(char), buffer_length[idx], fp);
+                        } else {
+                            break;
+                        }
+                        expectseqnum += 1;
+                    }
+                } else if (rcvdseqnum > expectseqnum && rcvdseqnum < (expectseqnum + N_receiver)) {
+                    idx = rcvdseqnum % (N_receiver-1);
+                    if (!ack_num[idx]){
+                        memcpy(buffer + (idx * PAYLOAD_SIZE), packet.payload, len);
+                        received_bytes += ret;
+                        ack_num[idx] = 1;
+                        buffer_length[idx] = len;
+                        sendACK(rcvdseqnum);
+                    }
+                } else if (rcvdseqnum < expectseqnum){
+                    sendACK(rcvdseqnum);
+                }
+            }
+        } else if ((packet.rtp.type == RTP_END) && (checksum == compute_checksum(&packet, sizeof(struct RTP_header)))) {
+            sendACK(packet.rtp.seq_num);
+            close(sockfd_receiver);
+            fclose(fp);
+            return received_bytes;
+        } else if (ret == -1){
+            fclose(fp);
+            return -1;
+        }
+    } 
+    fclose(fp);
+    return -1;
+}
+
+void terminateReceiver(){
+    close(sockfd_receiver);
+    return;
+}
---
+++ Lab2-RTP-Test/src/sender_def.c
@@ -0,0 +1,240 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "sys/socket.h"
+#include "netdb.h"
+#include "arpa/inet.h"
+#include "string.h"
+#include "rtp.h"
+#include "sender_def.h"
+#include "util.h"
+
+struct sockaddr_in recvaddr;
+int sockfd_sender;
+uint32_t N_sender, nextseqnum;
+
+
+void sendData(uint16_t length, uint32_t seq_num, char* payload){
+    struct RTP_packet packet;
+
+    packet.rtp.type = RTP_DATA;
+    packet.rtp.length = length;
+    packet.rtp.seq_num = seq_num;
+    packet.rtp.checksum = 0;
+    memcpy(packet.payload, payload, length);
+    packet.rtp.checksum = compute_checksum(&packet, sizeof(struct RTP_header) + length);
+
+    sendto(sockfd_sender, (char*)&packet, sizeof(struct RTP_header) + length, 0, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
+    return;
+}
+
+int initSender(const char* receiver_ip, uint16_t receiver_port, uint32_t window_size){
+    int rand_seq_num = rand();
+    struct RTP_header header;
+
+    sockfd_sender = socket(AF_INET, SOCK_DGRAM, 0);
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 100000;
+    setsockopt(sockfd_sender, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+    bzero(&recvaddr, sizeof(recvaddr));
+    recvaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, receiver_ip, &recvaddr.sin_addr);
+    recvaddr.sin_port = htons(receiver_port);
+
+    header.type = RTP_START;
+    header.seq_num = rand_seq_num;
+    header.length = 0;
+    header.checksum = 0;
+    header.checksum = compute_checksum(&header, sizeof(header));
+    sendto(sockfd_sender, &header, sizeof(header), 0, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
+
+    int recv = recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+    uint32_t checksum = header.checksum;
+    header.checksum = 0;
+    if ((recv > 0) && (header.type == RTP_ACK) && (checksum == compute_checksum(&header, sizeof(header)))){
+        N_sender = window_size;
+        return 0;
+    } else {
+        header.type = RTP_END;
+        header.length = 0;
+        header.seq_num = 0;
+        header.checksum = 0;
+        header.checksum = compute_checksum(&header, sizeof(header));
+        sendto(sockfd_sender, &header, sizeof(header), 0, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
+        recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+        return -1;
+    }
+}
+
+int sendMessage(const char* message){
+    FILE *fp = fopen(message, "rb");
+    if (fp == NULL){
+        printf("File does not exist.\n");
+        return -1;
+    }
+
+    struct RTP_header header;
+    fseek(fp, 0, SEEK_END);
+    unsigned long file_size = ftell(fp);
+    uint32_t packet_num = file_size / PAYLOAD_SIZE, base = 0, checksum;
+    uint32_t remainder = file_size - (packet_num * PAYLOAD_SIZE);               /* Length of the last data packet */
+    ssize_t rcv;
+    char *buffer = (char*)malloc(sizeof(char)*(PAYLOAD_SIZE * N_sender));
+    rewind(fp);
+
+    for (nextseqnum = 0; nextseqnum <= packet_num; nextseqnum++){
+        while(1){
+            if (nextseqnum < base + N_sender) {
+                fread(buffer + ((nextseqnum % N_sender) * PAYLOAD_SIZE), sizeof(char), (nextseqnum == packet_num) ? remainder : PAYLOAD_SIZE, fp);
+                sendData((nextseqnum == packet_num) ? remainder : PAYLOAD_SIZE, nextseqnum, buffer + ((nextseqnum % N_sender) * PAYLOAD_SIZE));
+                break;
+            } else {
+                rcv = recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+                checksum = header.checksum;
+                header.checksum = 0;
+                if (rcv == -1){
+                    for (int i = base; i < nextseqnum; i++){
+                        sendData((i == packet_num) ? remainder : PAYLOAD_SIZE, i, buffer + ((i % N_sender) * PAYLOAD_SIZE));
+                    }
+                } else if ((header.type == RTP_ACK) && (checksum == compute_checksum(&header, sizeof(header)))){
+                    if (header.seq_num > base){
+                        base = header.seq_num;
+                    }
+                }
+            }
+        }
+    }
+    fclose(fp);
+
+    if(base < nextseqnum){
+        while (1){
+            rcv = recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+            checksum = header.checksum;
+            header.checksum = 0;
+            if (rcv == -1){
+                for (int i = base; i < nextseqnum; i++){
+                    sendData((i == packet_num) ? remainder : PAYLOAD_SIZE, i, buffer + ((i % N_sender) * PAYLOAD_SIZE));
+                }
+            } else if ((header.type == RTP_ACK) && (checksum == compute_checksum(&header, sizeof(header)))){
+                if (header.seq_num > base){
+                    base = header.seq_num;
+                    if (base >= nextseqnum){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    if (nextseqnum > packet_num){
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+int sendMessageOpt(const char* message){
+    FILE *fp = fopen(message, "rb");
+    if (fp == NULL){
+        printf("File does not exist.\n");
+        return -1;
+    }
+
+    struct RTP_header header;
+    fseek(fp, 0, SEEK_END);
+    unsigned long file_size = ftell(fp);
+    uint32_t packet_num = file_size / PAYLOAD_SIZE, base = 0, checksum;
+    uint32_t remainder = file_size - (packet_num * PAYLOAD_SIZE);
+    ssize_t rcv;
+    char *buffer = (char*)malloc(sizeof(char)*(PAYLOAD_SIZE * N_sender));
+    uint8_t* ack_num = (uint8_t*)malloc(sizeof(uint8_t)*N_sender);
+    memset(ack_num, 0, N_sender);
+    rewind(fp);
+
+    for (nextseqnum = 0; nextseqnum <= packet_num; nextseqnum++){
+        while(1){
+            if (nextseqnum < base + N_sender) {
+                fread(buffer + ((nextseqnum % N_sender) * PAYLOAD_SIZE), sizeof(char), (nextseqnum == packet_num) ? remainder : PAYLOAD_SIZE, fp);
+                sendData((nextseqnum == packet_num) ? remainder : PAYLOAD_SIZE, nextseqnum, buffer + ((nextseqnum % N_sender) * PAYLOAD_SIZE));
+                break;
+            } else {
+                rcv = recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+                checksum = header.checksum;
+                header.checksum = 0;
+                if (rcv == -1){
+                    for (int i = base; i < nextseqnum; i++){
+                        if (!ack_num[i % N_sender])
+                            sendData((i == packet_num) ? remainder : PAYLOAD_SIZE, i, buffer + ((i % N_sender) * PAYLOAD_SIZE));
+                    }
+                } else if ((header.type == RTP_ACK) && (checksum == compute_checksum(&header, sizeof(header)))){
+                    if (header.seq_num > base && header.seq_num < (base + N_sender)){
+                        ack_num[header.seq_num % N_sender] = 1;
+                    } else if (header.seq_num == base){
+                        base += 1;
+                        for (int i = 1; i < N_sender; i++){
+                            if (!ack_num[base % N_sender]){
+                                break;
+                            }
+                            ack_num[base % N_sender] = 0;
+                            base += 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    fclose(fp);
+
+    if(base < nextseqnum){
+        while (1){
+            rcv = recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+            checksum = header.checksum;
+            header.checksum = 0;
+            if (rcv == -1){
+                for (int i = base; i < nextseqnum; i++){
+                    if (!ack_num[i % N_sender])
+                        sendData((i == packet_num) ? remainder : PAYLOAD_SIZE, i, buffer + ((i % N_sender) * PAYLOAD_SIZE));                    
+                }
+            } else if ((header.type == RTP_ACK) && (checksum == compute_checksum(&header, sizeof(header)))){
+                if (header.seq_num > base && header.seq_num < nextseqnum){
+                    ack_num[header.seq_num % N_sender] = 1;
+                } else if (header.seq_num == base){
+                    base += 1;
+                    for (int i = 1; i < N_sender; i++){
+                        if (!ack_num[base % N_sender]){
+                            break;
+                        }
+                        ack_num[base % N_sender] = 0;
+                        base += 1;
+                    }
+                }
+                if (base == nextseqnum){
+                    break;
+                }
+            }
+        }
+    }
+    
+    if (nextseqnum > packet_num){
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+void terminateSender(){
+    struct RTP_header header;
+
+    header.type = RTP_END;
+    header.length = 0;
+    header.seq_num = nextseqnum;
+    header.checksum = 0;
+    header.checksum = compute_checksum(&header, sizeof(header));
+
+    sendto(sockfd_sender, &header, sizeof(header), 0, (struct sockaddr *)&recvaddr, sizeof(recvaddr));
+    recvfrom(sockfd_sender, &header, sizeof(header), 0, NULL, NULL);
+
+    return;
+}
